e281aeacfd44cfcea89acbf3d7f99d5b0f85d435
Aaron Brooks <aaron.brooks@viasat.com>
2019-02-03 16:40:25 -0500

    DEBUG: Add expound and explicit spec.alpha dep.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  :url "https://github.com/clyfe/clara-eav/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
+                 [org.clojure/spec.alpha "0.2.168"]
                  [medley "1.0.0"]
                  [com.cerner/clara-rules "0.18.0"]]
   :plugins [[lein-cljsbuild "1.1.7" :exclusions [org.clojure/clojure]]
@@ -12,19 +13,24 @@
             [lein-cloverage "1.0.13"]]
   :codox {:metadata {:doc/format :markdown}}
   :doo {:paths {:rhino "lein run -m org.mozilla.javascript.tools.shell.Main"}}
-  
+
   :aliases
   {"build-cljs" ["with-profile" "test,provided" "cljsbuild" "once"]
    "test-cljs-just" ["with-profile" "test,provided" "doo" "rhino" "test" "once"]
    "test-cljs" ["do" ["build-cljs"] ["test-cljs-just"]]
    "test-all" ["do" ["test"] ["test-cljs"]]}
-  
+
   :profiles
   {:provided {:dependencies [[org.clojure/clojurescript "1.9.946"]]}
    :test {:dependencies [[lein-doo "0.1.10" :exclusions [org.clojure/clojure]]
                          [org.mozilla/rhino "1.7.7"]
                          [org.clojure/test.check "0.10.0-alpha3"]
-                         [tortue/spy "1.4.0"]]
+                         [tortue/spy "1.4.0"]
+                         [expound "0.7.1"]]
+          :injections [(do (require '[expound.alpha :as expound])
+                           (require '[clojure.spec.alpha :as s])
+                           (s/check-asserts true)
+                           #_(set! s/*explain-out* expound/printer))]
           :cljsbuild {:builds [{:id "test"
                                 :source-paths ["src" "test"]
                                 :compiler {:output-to "target/main.js"
--- test/clara_eav/test_helper.cljc
+++ test/clara_eav/test_helper.cljc
@@ -1,11 +1,14 @@
 (ns clara-eav.test-helper
-  (:require
+  (:require [expound.alpha :as expound]
+    #?@(:clj [[clojure.spec.alpha :as s]]
+        :cljs [[cljs.spec.alpha :as s]])
     #?@(:clj [[clojure.spec.test.alpha :as st]]
         :cljs [[cljs.spec.test.alpha :as st]])))
 
 (defn spec-fixture [f]
   (st/instrument)
-  (f)
+  (binding [#_#_s/*explain-out* expound/printer]
+    (f))
   (st/unstrument))
 
 (defmulti strip-ids (fn [s] (type s)))
